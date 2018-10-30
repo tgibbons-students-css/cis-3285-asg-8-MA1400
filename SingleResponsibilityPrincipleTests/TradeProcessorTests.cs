6cb2c792f3439acfbb9bc5a64af00193ed30c844
Malik Alwan <malwan1@css.edu>
2018-10-30 11:02:46 -0500

    test methods

--- SingleResponsibilityPrincipleTests/TradeProcessorTests.cs
+++ SingleResponsibilityPrincipleTests/TradeProcessorTests.cs
@@ -2,6 +2,7 @@
 using SingleResponsibilityPrinciple;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Reflection;
@@ -16,26 +17,26 @@ namespace SingleResponsibilityPrinciple.Tests
         private int CountDbRecords()
         {
             using (var connection = new System.Data.SqlClient.SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\malwan1\source\repos\cis-3285-asg-8-MA1400\tradedatabase.mdf;Integrated Security=True;Connect Timeout=30"))
-            {
-                connection.Open();
+            {   if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+                
                 string myScalarQuery = "SELECT COUNT(*) FROM trade";
                 SqlCommand myCommand = new SqlCommand(myScalarQuery, connection);
-                myCommand.Connection.Open();
+               // myCommand.Connection.Open();
                 int count = (int)myCommand.ExecuteScalar();
                 connection.Close();
                 return count;
             }
         }
-        [TestMethod()]
-        public void ProcessTradesTest()
-        {
-            Assert.Fail();
-        }
+       
+       
         [TestMethod()]
         public void TestNormalFile()
         {
             //Arrange
-            var tradeStram = Assembly.GetExecutingAssembly().GetManifestResourceStream("singleRepositoryPrincipleTest.goodtrades.txt");
+            var tradeStram = Assembly.GetExecutingAssembly().GetManifestResourceStream("SingleResponsibilityPrincipleTests.goodtrades.txt");
             var TradeProcessor = new TradeProcessor();
 
             //Act
@@ -46,5 +47,38 @@ namespace SingleResponsibilityPrinciple.Tests
             int countAfter = CountDbRecords();
             Assert.AreEqual(countBefore +4, countAfter);
         }
+       
+
+        [TestMethod()]
+    public void TestBadFile()
+    {
+        //Arrange
+        var tradeStram = Assembly.GetExecutingAssembly().GetManifestResourceStream("SingleResponsibilityPrincipleTests.badtrades.txt");
+        var TradeProcessor = new TradeProcessor();
+
+        //Act
+        int countBefore = CountDbRecords();
+        TradeProcessor.ProcessTrades(tradeStram);
+
+        //Assert
+        int countAfter = CountDbRecords();
+        Assert.AreEqual(countBefore, countAfter);
+      }
+        [TestMethod()]
+        public void TestNegative()
+        {
+            //Arrange
+            var tradeStram = Assembly.GetExecutingAssembly().GetManifestResourceStream("SingleResponsibilityPrincipleTests.negativetrades.txt");
+            var TradeProcessor = new TradeProcessor();
+            //Act
+
+            int countBefore = CountDbRecords();
+            TradeProcessor.ProcessTrades(tradeStram);
+
+            //Assert
+            int countAfter = CountDbRecords();
+            Assert.AreEqual(countBefore, countAfter);
+        }
     }
+
 }
